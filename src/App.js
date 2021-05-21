071a4194e4e2638ce4c45204e9ea23224c94c9d7
GabrielWeinbrenner <gabriel.weinbrenner@gmail.com>
2021-05-21 15:11:16 -0400

    browser router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./static/Header";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Landing from "./pages/Landing";
+import MemeTemplate from "./pages/MemeTemplate";
+
+// import theme from "./ui/Theme";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" render={() => <Landing />} />
+      </Switch>
+    </BrowserRouter>
+    // </ThemeProvider>
   );
 }
 
